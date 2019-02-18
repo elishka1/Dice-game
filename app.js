7984d566163ec48008aaece0dec1d2d0f764ee46
Code Nation 22 <codenation@CN-22.lan>
2019-02-18 17:11:03 +0000

    ooo

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 
-const dice1= document.getElementById("dice1");
 
-document.addEventListener ("click", () => {
-    dice1.style.color('red');
-})
+ let score
+
+ function roll () {
+  score= Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  document.getElementById("dicescore").innerHTML = score
+ }
+ 
+if (dicescore==1) {
+    document.getElementById("dice").src="./img/dice1.png";
+}
+
+else if (dicescore==2) {
+    document.getElementById("dice").src="./img/dice2.png";
+}
+
+else if (dicescore==3) {
+    document.getElementById("dice").src="./img/dice3.png";
+}
+
+else if (dicescore==4) {
+    document.getElementById("dice").src="./img/dice4.png";
+}
+
+else if (dicescore==5) {
+    document.getElementById("dice").src="./img/dice5.png";
+}
+
+
