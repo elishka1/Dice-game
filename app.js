71e1ff771ab2fc951ab1e4f22a6fd9490663b636
Code Nation 22 <codenation@CN-22.lan>
2019-02-25 17:00:30 +0000

    d

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 
 
- let score
+ let dicescore
+
+ let score = 0
 
  function roll () {
-  score= Math.floor(Math.random() * (6 - 1 + 1) + 1);
-  document.getElementById("dicescore").innerHTML = score
+  dicescore= Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  document.getElementById("dicescore").innerHTML = dicescore
+
+  score += dicescore;
+  document.getElementById("score").innerHTML = score
+
+if (score>=20) {
+    document.getElementById("dicescore").innerHTML = "You Win!"
+    score=0
  }
- 
+
 if (dicescore==1) {
     document.getElementById("dice").src="./img/dice1.png";
+    document.getElementById("dicescore").innerHTML = "You're Out!"
+    score=0
+    
 }
 
 else if (dicescore==2) {
@@ -26,5 +38,9 @@ else if (dicescore==4) {
 else if (dicescore==5) {
     document.getElementById("dice").src="./img/dice5.png";
 }
+else {
+    document.getElementById("dice").src="./img/dice6.png";
+}
 
+ }
 
